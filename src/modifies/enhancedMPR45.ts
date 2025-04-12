494135ccb53d1c9b89c1a74d0f2302bcba02e4fd
高耳鸡 <703821945@qq.com>
2025-04-12 14:43:28 +0800

    adjust MPR45

--- src/modifies/enhancedMPR45.ts
+++ src/modifies/enhancedMPR45.ts
@@ -2,7 +2,6 @@ import {IDatabaseTables} from '@spt/models/spt/server/IDatabaseTables';
 import {ILogger} from '@spt/models/spt/utils/ILogger';
 import {ItemHelper} from '@spt/helpers/ItemHelper';
 import {ItemTpl} from '@spt/models/enums/ItemTpl';
-import {BaseClasses} from '../../types/models/enums/BaseClasses';
 
 export default function enhancedMPR45(logger:ILogger,itemHelper:ItemHelper,tables: IDatabaseTables): void {
   const mpr45 = tables.templates.items[ItemTpl.MOUNT_NCSTAR_MPR45_BACKUP] || null;
@@ -17,7 +16,13 @@ export default function enhancedMPR45(logger:ILogger,itemHelper:ItemHelper,table
     return;
   }
 
-  mpr45._props.Slots[0]._props.filters[0].Filter.push(ItemTpl.MOUNT_LARUE_LT101_QD_TACTICAL_PICATINNY_RISER,BaseClasses.IRON_SIGHT);
+  mpr45._props.Slots[0]._props.filters[0].Filter.push(
+    ItemTpl.MOUNT_LARUE_LT101_QD_TACTICAL_PICATINNY_RISER,
+    //ItemTpl.IRONSIGHT_SOK12_CSS_REAR_SIGHT_RAIL_MOUNT,
+    //ItemTpl.IRONSIGHT_P226_SIGHT_MOUNT_220239_REAR_SIGHT_BEARING,
+    //ItemTpl.IRONSIGHT_AK_TAKTIKA_TULA_TT01_REAR_SIGHT_RAIL,
+    //ItemTpl.IRONSIGHT_M9A3_SIGHT_MOUNT_REAR_SIGHT_RAIL,
+  );
 
   for (const id in tables.templates.items) {
     const template = tables.templates.items[id] || null;
