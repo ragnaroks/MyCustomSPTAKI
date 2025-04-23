33f466fd38012a0272ef4f54b56ed039768669d5
高耳鸡 <703821945@qq.com>
2025-04-23 20:54:38 +0800

    misc

--- src/modifies/modifyBaseClassSimpleContainer.ts
+++ src/modifies/modifyBaseClassSimpleContainer.ts
@@ -5,7 +5,28 @@ import {ILogger} from '@spt/models/spt/utils/ILogger';
 import {ItemHelper} from '@spt/helpers/ItemHelper';
 
 export default function modifyBaseClassSimpleContainer(logger:ILogger,itemHelper:ItemHelper,tables:IDatabaseTables) : void {
-  const idArray = itemHelper.getItemTplsOfBaseType(BaseClasses.SIMPLE_CONTAINER);
+  const idArray = [
+    ItemTpl.CONTAINER_AMMUNITION_CASE,
+    ItemTpl.CONTAINER_BALLISTIC_PLATE_CASE,
+    //ItemTpl.CONTAINER_DOCUMENTS_CASE,
+    ItemTpl.CONTAINER_DOGTAG_CASE,
+    //ItemTpl.CONTAINER_GINGY_KEYCHAIN,
+    ItemTpl.CONTAINER_GRENADE_CASE,
+    //ItemTpl.CONTAINER_INJECTOR_CASE,
+    ItemTpl.CONTAINER_ITEM_CASE,
+    //ItemTpl.CONTAINER_KEY_TOOL,
+    //ItemTpl.CONTAINER_KEYCARD_HOLDER_CASE,
+    ItemTpl.CONTAINER_MAGAZINE_CASE,
+    ItemTpl.CONTAINER_MEDICINE_CASE,
+    ItemTpl.CONTAINER_MONEY_CASE,
+    ItemTpl.CONTAINER_MR_HOLODILNICK_THERMAL_BAG,
+    ItemTpl.CONTAINER_SICC,
+    //ItemTpl.CONTAINER_STREAMER_ITEM_CASE,
+    ItemTpl.CONTAINER_WEAPON_CASE,
+    //ItemTpl.CONTAINER_SIMPLE_WALLET,
+    //ItemTpl.CONTAINER_WZ_WALLET,
+  ];
+
   for(const id of idArray) {
     const template = tables.templates.items[id] || null;
     if(!template || template._type !== "Item") {continue;}
@@ -17,7 +38,12 @@ export default function modifyBaseClassSimpleContainer(logger:ILogger,itemHelper
     template._props.Grids[0]._props.cellsV = 14;
   }
 
-  const idArray2 = [ItemTpl.CONTAINER_LUCKY_SCAV_JUNK_BOX];
+  const idArray2 = [
+    ItemTpl.CONTAINER_LUCKY_SCAV_JUNK_BOX,//3.12 的时候移到上面去
+    //ItemTpl.CONTAINER_THICC_ITEM_CASE, 3.12 的时候再应用
+    //ItemTpl.CONTAINER_THICC_WEAPON_CASE, 3.12 的时候再应用
+  ];
+
   for(const id of idArray2) {
     const template = tables.templates.items[id] || null;
     if(!template || template._type !== "Item") {continue;}
