b162d15591a6ed581f2d9584fa83e6a8b3d0103d
高耳鸡 <703821945@qq.com>
2026-01-24 22:56:12 +0800

    --

--- Modifies/ModifyGlobalDatabase.cs
+++ Modifies/ModifyGlobalDatabase.cs
@@ -1,16 +1,13 @@
 ﻿using SPTarkov.DI.Annotations;
-using SPTarkov.Server.Core.Constants;
 using SPTarkov.Server.Core.DI;
-using SPTarkov.Server.Core.Models.Common;
 using SPTarkov.Server.Core.Models.Eft.Common;
-using SPTarkov.Server.Core.Models.Eft.Common.Tables;
 using SPTarkov.Server.Core.Models.Logging;
 using SPTarkov.Server.Core.Models.Spt.Logging;
+using SPTarkov.Server.Core.Models.Spt.Server;
 using SPTarkov.Server.Core.Models.Utils;
 using SPTarkov.Server.Core.Services;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyCustomSPTAKI.Modifies;
@@ -29,7 +26,8 @@ public class ModifyGlobalDatabase : IOnLoad {
 
     public Task OnLoad () {
         Globals globals = this.DatabaseService.GetGlobals();
-        Dictionary<MongoId, TemplateItem> templates = this.DatabaseService.GetItems();
+        Dictionary<String, Location> locations = this.DatabaseService.GetLocations().GetDictionary();
+        //Dictionary<MongoId, TemplateItem> templates = this.DatabaseService.GetItems();
 
         //globals.Configuration.BaseCheckTime = 0D;
         //globals.Configuration.BaseLoadTime = 0D;
@@ -41,6 +39,7 @@ public class ModifyGlobalDatabase : IOnLoad {
         globals.Configuration.SkillFreshEffectiveness = 3D;
         globals.Configuration.SavagePlayCooldown = 60;
 
+        
         foreach (MaxActiveOfferCount maxActiveOfferCount in globals.Configuration.RagFair.MaxActiveOfferCount) {
             maxActiveOfferCount.Count *= 10;
             maxActiveOfferCount.CountForSpecialEditions *= 10;
@@ -48,6 +47,22 @@ public class ModifyGlobalDatabase : IOnLoad {
 
         // 物品无同时上架数量限制
         globals.Configuration.RagFair.ItemRestrictions = [];
+
+        // 战局时间 4 倍，撤离点无条件
+        foreach (KeyValuePair<String, Location> location in locations) {
+            if(location.Key.Equals("Develop", StringComparison.InvariantCultureIgnoreCase)) {continue;}
+            if(location.Key.Equals("Hideout", StringComparison.InvariantCultureIgnoreCase)) {continue;}
+            if(location.Key.Equals("PrivateArea", StringComparison.InvariantCultureIgnoreCase)) {continue;}
+            if(location.Key.Equals("Suburbs", StringComparison.InvariantCultureIgnoreCase)) {continue;}
+            if(location.Key.Equals("Terminal", StringComparison.InvariantCultureIgnoreCase)) {continue;}
+            if(location.Key.Equals("Town", StringComparison.InvariantCultureIgnoreCase)) {continue;}
+            location.Value.Base.EscapeTimeLimit *= 4;
+            foreach (AllExtractsExit extractsExit in location.Value.AllExtracts) {
+                extractsExit.PassageRequirement = SPTarkov.Server.Core.Models.Enums.RequirementState.None;
+                extractsExit.ExfiltrationType = SPTarkov.Server.Core.Models.Enums.ExfiltrationType.Individual;
+            }
+        }
+        
 
         this.Logger.Log(
             LogLevel.Info,
---
+++ Modifies/ModifyPlayerHealth.cs
@@ -0,0 +1,52 @@
+﻿using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Helpers;
+using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Models.Eft.Profile;
+using SPTarkov.Server.Core.Models.Logging;
+using SPTarkov.Server.Core.Models.Spt.Logging;
+using SPTarkov.Server.Core.Models.Utils;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyCustomSPTAKI.Modifies;
+
+[Injectable(InjectionType.Scoped, null, OnLoadOrder.PostSptModLoader + 1)]
+public class ModifyPlayerHealth : IOnLoad {
+    private ISptLogger<ModifyPlayerHealth> Logger { get; }
+    private ProfileHelper ProfileHelper { get; }
+
+#pragma warning disable IDE0290 // 使用主构造函数    
+    public ModifyPlayerHealth (ISptLogger<ModifyPlayerHealth> logger, ProfileHelper profileHelper) {
+        this.Logger = logger;
+        this.ProfileHelper = profileHelper;
+    }
+#pragma warning restore IDE0290 // 使用主构造函数
+
+    public Task OnLoad () {
+        Dictionary<MongoId, SptProfile> profiles = this.ProfileHelper.GetProfiles();
+        foreach (KeyValuePair<MongoId, SptProfile> profile in profiles) {
+            BotBaseHealth? botBaseHealth = profile.Value.CharacterData?.PmcData?.Health;
+            if (botBaseHealth is null || botBaseHealth.BodyParts is null) { continue; }
+            foreach (KeyValuePair<String, BodyPartHealth> bodyPart in botBaseHealth.BodyParts) {
+                CurrentMinMax? current = bodyPart.Value.Health;
+                if (current is null) { continue; }
+                current.Minimum = current.Maximum = 200;
+            }
+            this.Logger.Log(
+                LogLevel.Info,
+                String.Concat(Constants.LoggerPrefix, "ModifyPlayerHealth.OnLoad() / ", profile.Value.ProfileInfo?.Username, " / 已调整生命值"),
+                LogTextColor.White
+            );
+        }
+
+        this.Logger.Log(
+            LogLevel.Info,
+            String.Concat(Constants.LoggerPrefix, "ModifyPlayerHealth.OnLoad() / success"),
+            LogTextColor.Green
+        );
+        return Task.CompletedTask;
+    }
+}
